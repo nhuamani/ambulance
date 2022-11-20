ca8e31d561207f5c75b800fd75dae6b26801f365
nhuamani <nhuamani.c7@gmail.com>
2022-11-20 14:29:01 -0500

    create medic

--- projects/system/src/app/modules/medic/infrastructure/medic.infrastruture.ts
+++ projects/system/src/app/modules/medic/infrastructure/medic.infrastruture.ts
@@ -3,6 +3,7 @@ import { MedicRepository } from '../domain/repositories/medic.repository';
 
 export class MedicInfrastructure implements MedicRepository {
   save(medic: Medic): Promise<void> {
+    console.log('Medic saved', medic);
     return Promise.resolve();
   }
 }
--- projects/system/src/app/modules/medic/interfaces/views/form/form.component.ts
+++ projects/system/src/app/modules/medic/interfaces/views/form/form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MedicApplication } from '../../../application/medic.aplication';
+import { MedicRepository } from '../../../domain/repositories/medic.repository';
+import { MedicInfrastructure } from '../../../infrastructure/medic.infrastruture';
+
 @Component({
   selector: 'amb-form',
   templateUrl: './form.component.html',
@@ -9,7 +13,17 @@ export class FormComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    const infrastructure: MedicRepository = new MedicInfrastructure();
+    const application = new MedicApplication(infrastructure);
+
+    await application.createMedic(
+      'Juan',
+      'Carrasco',
+      '134',
+      '999999999',
+      'email@correo.com'
+    );
   }
 
 }
